0ff19cc1bd412286192dd2d48d6604a3e95eea6e
Joao Toledo <j.toledo.rocha96@gmail.com>
2023-08-07 18:40:44 -0300

    did bit of polishing on contact section. Started working on responsivness

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,15 +2,15 @@ import { Application } from "@hotwired/stimulus";
 import CustomController from "./custom_controller";
 import TypedController from "./typed_controller";
 import LogoAnimate from "./logo_animation_controller"
+import HamburgerController from "./hamburger_controller";
 
 const application = Application.start();
 application.register("custom", CustomController);
 application.register("typed", TypedController);
-application.register("logoAnimation",LogoAnimate)
+application.register("logoAnimation",LogoAnimate);
+application.register("hamburger",HamburgerController);
 
 
- document.addEventListener("DOMContentLoaded", function() {
-   LogoAnimate();
- });
+
 
 export { application };
