340f01d5d07146cc2dc8c22cbe35ff3d224c19d2
Joao Toledo <j.toledo.rocha96@gmail.com>
2023-08-28 16:19:02 -0300

    changed bio text and in-depth description of seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,29 +8,30 @@
 
 Project.destroy_all
 
+mentormeet = Project.create(
+  name:'MentorMeet',
+  description:'A collaborative project that consists in a mentorship marketplace. Users can either offer or hire mentorships.',
+  url:"http://meetmentor.herokuapp.com",
+  image:'MentorMeetLogo.png',
+  in_depth_description: "In my first team project, we developed a browser application modeled after popular marketplaces like Airbnb. Built on the Ruby on Rails framework, this project expanded my knowledge regarding more complex databases, join tables, and nested structures. JavaScript functionalities were integrated for enhanced interactivity.You can check my contributions to this project on GitHub.",
+  modal_image:'MentorMeetModal1.png',
+  modal_image_2:'MentorMeetModal2.png',
+  modal_image_3:'MentorMeetModal3.png',
+  gh_repo:"https://github.com/patricksanto/mentormeet"
+)
+
 telecosmo = Project.create(
   name: 'TeleCosmo',
   description: 'A collaborative Streaming mobile application for engaging youngsters in literature and story telling',
   url: 'http://www.telecosmo.com.br/',
   image: 'TeleCosmoLogo.png',
-  in_depth_description: 'As for the final group project in my bootcamp ,my team decided to go for a mobile application.Drawing inspiration from the intuitive UI/UX of renowned streaming services , we came up with a multifaceted product that works not only as a streaming platform ,but included social components, enabling users to establish profiles, cultivate friendships, personalize avatars, and engage in dynamic chatrooms. My contributions can be checked on GitHub. All art was envisioned by Sonhorama.',
+  in_depth_description: 'On my second team project, we decided to develop a mobile application.Drawing inspiration from the intuitive UI/UX of renowned streaming services , we came up with a multifaceted product that works not only as a streaming platform ,but included social components, enabling users to establish profiles, cultivate friendships, personalize avatars, and engage in dynamic chatrooms. My contributions can be checked on GitHub. All art was envisioned by Sonhorama.',
   modal_image: 'TelecosmoModal1.png',
   modal_image_2: 'TelecosmoModal2.png',
   modal_image_3: 'TelecosmoModal3.png',
   gh_repo: 'https://github.com/dlameira/Telecosmo'
 )
 
-mentormeet = Project.create(
-  name:'MentorMeet',
-  description:'A collaborative project that consists in a mentorship marketplace. Users can either offer or hire mentorships.',
-  url:"http://meetmentor.herokuapp.com",
-  image:'MentorMeetLogo.png',
-  in_depth_description: "In my debut team project, we developed a browser application modeled after popular marketplaces like Airbnb. Built on the Ruby on Rails framework, this project expanded my knowledge regarding more complex databases, join tables, and nested structures. JavaScript functionalities were integrated for enhanced interactivity.You can check my contributions to this project on GitHub",
-  modal_image:'MentorMeetModal1.png',
-  modal_image_2:'MentorMeetModal2.png',
-  modal_image_3:'MentorMeetModal3.png',
-  gh_repo:"https://github.com/patricksanto/mentormeet"
-)
 
 movietime = Project.create(
   name:'MovieTime',
